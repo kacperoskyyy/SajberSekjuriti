e106faba3415b17ffa6cd228a3c937aed49ce44e
kacperoskyyy <54751537+kacperoskyyy@users.noreply.github.com>
2025-10-11 18:31:30 +0200

    zmiana proejktu xd
    szybka zmiana :>

--- SajberSekjuriti/SajberSekjuriti/Program.cs
+++ SajberSekjuriti/SajberSekjuriti/Program.cs
@@ -1,35 +1,58 @@
-using SajberSekjuriti.Client.Pages;
-using SajberSekjuriti.Components;
+using MongoDB.Driver;
+using SajberSekjuriti.Data; 
+using SajberSekjuriti.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddInteractiveServerComponents()
-    .AddInteractiveWebAssemblyComponents();
+builder.Services.AddRazorPages();
+
+// Konfiguracja MongoDB
+builder.Services.AddSingleton<IMongoClient>(s => new MongoClient(builder.Configuration["MongoDbSettings:ConnectionString"]));
+// Nasze serwisy
+builder.Services.AddScoped<UsersService>();
+builder.Services.AddScoped<PasswordService>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+// Tworzenie domyœlnego admina
+using (var scope = app.Services.CreateScope())
 {
-    app.UseWebAssemblyDebugging();
+    var services = scope.ServiceProvider;
+    var usersService = services.GetRequiredService<UsersService>();
+    var passwordService = services.GetRequiredService<PasswordService>();
+    var adminUser = await usersService.GetByUsernameAsync("ADMIN");
+    if (adminUser == null)
+    {
+        var newAdmin = new User
+        {
+            Username = "ADMIN",
+            FullName = "Administrator",
+            PasswordHash = passwordService.HashPassword("Admin123!"),
+            Role = UserRole.Admin,
+            PasswordLastSet = DateTime.UtcNow,
+            MustChangePassword = true
+        };
+        await usersService.CreateAsync(newAdmin);
+        Console.WriteLine("Stworzono konto admina.");
+    }
 }
-else
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
 {
-    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
-
 app.UseStaticFiles();
-app.UseAntiforgery();
 
-app.MapRazorComponents<App>()
-    .AddInteractiveServerRenderMode()
-    .AddInteractiveWebAssemblyRenderMode()
-    .AddAdditionalAssemblies(typeof(SajberSekjuriti.Client._Imports).Assembly);
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapRazorPages();
 
 app.Run();
---
+++ SajberSekjuriti/SajberSekjuriti/Services/UsersService.cs
@@ -0,0 +1,17 @@
+﻿using MongoDB.Driver;
+namespace SajberSekjuriti.Services
+{
+    public class UsersService
+    {
+        private readonly IMongoCollection<User> _users;
+        public UsersService(IMongoClient client, IConfiguration config)
+        {
+            var dbName = config["MongoDbSettings:DatabaseName"];
+            var colName = config["MongoDbSettings:UsersCollectionName"];
+            var database = client.GetDatabase(dbName);
+            _users = database.GetCollection<User>(colName);
+        }
+        public async Task<User?> GetByUsernameAsync(string username) => await _users.Find(u => u.Username == username).FirstOrDefaultAsync();
+        public async Task CreateAsync(User user) => await _users.InsertOneAsync(user);
+    }
+}
